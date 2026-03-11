498e81428ecaa57bdb5182b6aeb27bb75658f0bd
Varun N <varun.segment@gmail.com>
2026-03-11 19:18:56 +0530

    rustlings - quiz 2
    Used a loop to append string for `times` operation, however discovered
    that there's already a method to do so, how nice.

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,21 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output: Vec<String> = Vec::new();
+
+        for (string, command) in input {
+            let out: String = match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().to_string(),
+                Command::Append(times) => string + &"bar".repeat(times),
+            };
+
+            output.push(out);
+        }
+
+        output
+    }
 }
 
 fn main() {
@@ -36,6 +50,8 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    use crate::my_module::transformer;
+
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
     use super::Command;
