f8bd25a9ed019100826a20668abdb32acdc963b1
Varun N <varun.segment@gmail.com>
2026-03-11 14:10:54 +0530

    rustlings - hashmap exercises solved

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -27,6 +27,32 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
         let team_1_score: u8 = split_iterator.next().unwrap().parse().unwrap();
         let team_2_score: u8 = split_iterator.next().unwrap().parse().unwrap();
 
+        scores
+            .entry(team_1_name)
+            .and_modify(|team_score| {
+                // team-score has scored and conceded as struct fields,
+                // since it exists in the map, and we new values, update for team 1 and team 2
+                team_score.goals_scored += team_1_score;
+                team_score.goals_conceded += team_2_score;
+            })
+            .or_insert(TeamScores {
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score,
+            });
+
+        scores
+            .entry(team_2_name)
+            .and_modify(|team_score| {
+                // team-score has scored and conceded as struct fields,
+                // since it exists in the map, and we new values, update for team 1 and team 2
+                team_score.goals_scored += team_2_score;
+                team_score.goals_conceded += team_1_score;
+            })
+            .or_insert(TeamScores {
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score,
+            });
+
         // TODO: Populate the scores table with the extracted details.
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
