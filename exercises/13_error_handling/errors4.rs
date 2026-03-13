d1a103f9e7547238cbd2c7b173c0915bfa2086a6
Varun N <varun.segment@gmail.com>
2026-03-13 15:50:34 +0530

    rustlings - error handling exercises completed

--- exercises/13_error_handling/errors4.rs
+++ exercises/13_error_handling/errors4.rs
@@ -11,7 +11,12 @@ impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<Self, CreationError> {
         // TODO: This function shouldn't always return an `Ok`.
         // Read the tests below to clarify what should be returned.
-        Ok(Self(value as u64))
+        match value.cmp(&0) {
+            std::cmp::Ordering::Less => Err(CreationError::Negative),
+            std::cmp::Ordering::Equal => Err(CreationError::Zero),
+            std::cmp::Ordering::Greater => Ok(Self(value as u64)),
+        }
+        // Ok(Self(value as u64))
     }
 }
 
