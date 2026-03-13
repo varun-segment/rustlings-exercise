6571e8ca31d6ab10fffe6e3b82187b46b641f9f8
Varun N <varun.segment@gmail.com>
2026-03-13 20:41:08 +0530

    rustlings - trait exercises completed

--- exercises/15_traits/traits5.rs
+++ exercises/15_traits/traits5.rs
@@ -18,8 +18,8 @@ struct OtherStruct;
 impl SomeTrait for OtherStruct {}
 impl OtherTrait for OtherStruct {}
 
-// TODO: Fix the compiler error by only changing the signature of this function.
-fn some_func(item: ???) -> bool {
+// Fix the compiler error by only changing the signature of this function.
+fn some_func<T: SomeTrait + OtherTrait>(item: T) -> bool {
     item.some_function() && item.other_function()
 }
 
