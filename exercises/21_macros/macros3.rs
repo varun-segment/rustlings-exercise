f8473dc2cc995b59f69b1c891c83921509fc7f72
Varun N <varun.segment@gmail.com>
2026-04-16 18:47:34 +0530

    rustlings - macro exercises complete

--- exercises/21_macros/macros3.rs
+++ exercises/21_macros/macros3.rs
@@ -1,6 +1,7 @@
 // TODO: Fix the compiler error without taking the macro definition out of this
 // module.
 mod macros {
+    #[macro_export] // This makes it available at the crate root
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
